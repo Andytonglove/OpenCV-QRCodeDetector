ed141f9ae6c0147ecf1f0b0750edfd48392c550a
ParatrooperAndy <1468492471@qq.com>
2024-05-24 11:13:51 +0800

    update res

--- qrcode-file-tx-main/pickle2mp4.py
+++ qrcode-file-tx-main/pickle2mp4.py
@@ -5,10 +5,11 @@ import numpy as np
 from PIL import Image
 
 # 设置参数
-FRAME_RATE = 5  # 帧率为每秒5帧
-VIDEO_FILE_NAME = "qrcode_video_28.mp4"
+FRAME_RATE = 2  # 帧率为每秒5帧
+VIDEO_FILE_NAME = "qrcode_video.mp4"
 QRCODE_NUMBER = 28
 QR_CODE_NUMBER_PER_ROW = 7
+QR_CODE_NUMBER_PER_COL = 4
 
 # 从 pickle 文件加载数据
 with gzip.open("qr_cache.pickle", "rb") as f:
@@ -37,19 +38,21 @@ qr_width, qr_height = first_qr_image.size
 def create_page_image(frame_idx):
     page_image = Image.new(
         "RGB",
-        (
-            qr_width * QR_CODE_NUMBER_PER_ROW,
-            qr_height * (QRCODE_NUMBER // QR_CODE_NUMBER_PER_ROW),
-        ),
+        (qr_width * QR_CODE_NUMBER_PER_ROW, qr_height * QR_CODE_NUMBER_PER_COL),
         "white",
     )
     for i in range(QRCODE_NUMBER):
-        chunk_index = i // (frames_number + 1)
-        if frame_idx < len(chunks[chunk_index]):
-            qr_image = chunks[chunk_index][frame_idx].convert("RGB")
-            x = (i % QR_CODE_NUMBER_PER_ROW) * qr_width
-            y = (i // QR_CODE_NUMBER_PER_ROW) * qr_height
-            page_image.paste(qr_image, (x, y))
+        chunk_index = i + frame_idx * QRCODE_NUMBER
+        if chunk_index < len(chunks) * frames_number:
+            chunk_idx = chunk_index // frames_number
+            frame_idx_within_chunk = chunk_index % frames_number
+            qr_image = chunks[chunk_idx][frame_idx_within_chunk].convert("RGB")
+            open_cv_image = cv2.cvtColor(np.array(qr_image), cv2.COLOR_RGB2BGR)
+            qr_image = Image.fromarray(open_cv_image)
+            x = i % QR_CODE_NUMBER_PER_ROW
+            y = i // QR_CODE_NUMBER_PER_ROW
+            page_image.paste(qr_image, (x * qr_width, y * qr_height))
+
     return page_image
 
 
@@ -58,14 +61,14 @@ video_writer = cv2.VideoWriter(
     VIDEO_FILE_NAME,
     cv2.VideoWriter_fourcc(*"mp4v"),  # 使用 'mp4v' 编码器生成 mp4 文件
     FRAME_RATE,
-    (
-        qr_width * QR_CODE_NUMBER_PER_ROW,
-        qr_height * (QRCODE_NUMBER // QR_CODE_NUMBER_PER_ROW),
-    ),
+    (qr_width * QR_CODE_NUMBER_PER_ROW, qr_height * QR_CODE_NUMBER_PER_COL),
 )
 
+# 计算总帧数
+total_frames = (len(chunks) * frames_number) // QRCODE_NUMBER + 1
+
 # 写入视频文件
-for frame_idx in range(frames_number + 1):
+for frame_idx in range(total_frames):
     page_image = create_page_image(frame_idx)
     open_cv_image = cv2.cvtColor(np.array(page_image), cv2.COLOR_RGB2BGR)
     video_writer.write(open_cv_image)
