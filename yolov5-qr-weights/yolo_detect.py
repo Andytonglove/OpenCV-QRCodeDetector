c51bb2be1cd2a95eb182f69e4f4b27c032a1f099
ParatrooperAndy <1468492471@qq.com>
2024-06-03 17:40:24 +0800

    add expl

--- yolov5-qr-weights/yolo_detect.py
+++ yolov5-qr-weights/yolo_detect.py
@@ -3,6 +3,30 @@ import time
 import sys
 import numpy as np
 
+import argparse
+
+
+def get_args():
+    """
+    Parse command line arguments and return the parsed arguments.
+
+    Returns:
+        argparse.Namespace: The parsed command line arguments.
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--input", type=str, help="path to input image or video file")
+    parser.add_argument(
+        "--output",
+        type=str,
+        help="path to save output image or video file",
+        default="output.jpg",  # 默认输出文件名.avi也可
+    )
+
+    args = parser.parse_args()
+
+    return args
+
 
 def build_model(is_cuda):
     """
@@ -35,9 +59,20 @@ INPUT_HEIGHT = 640
 SCORE_THRESHOLD = 0.2
 NMS_THRESHOLD = 0.4
 CONFIDENCE_THRESHOLD = 0.4
+class_list = ["QR"]
 
 
 def detect(image, net):
+    """
+    Detects objects in an image using a pre-trained neural network.
+
+    Args:
+        image (numpy.ndarray): The input image.
+        net: The pre-trained neural network.
+
+    Returns:
+        numpy.ndarray: The predictions of the neural network.
+    """
     blob = cv2.dnn.blobFromImage(
         image, 1 / 255.0, (INPUT_WIDTH, INPUT_HEIGHT), swapRB=True, crop=False
     )
@@ -47,21 +82,47 @@ def detect(image, net):
 
 
 def load_capture(video_path):
+    """
+    Loads a video capture object from the specified video path.
+
+    Parameters:
+    video_path (str): The path to the video file.
+
+    Returns:
+    capture (cv2.VideoCapture): The video capture object.
+
+    """
     capture = cv2.VideoCapture(video_path)
     return capture
 
 
 def load_classes():
+    """
+    Load the list of classes from the 'classes.txt' file.
+
+    Returns:
+        class_list (list): A list of class names.
+
+    """
     class_list = []
     with open("classes.txt", "r") as f:
         class_list = [cname.strip() for cname in f.readlines()]
     return class_list
-
-
-class_list = load_classes()
+    # class_list = load_classes()
 
 
 def wrap_detection(input_image, output_data):
+    """
+    Detects objects in an input image using YOLOv5 model.
+
+    Args:
+        input_image (numpy.ndarray): The input image.
+        output_data (numpy.ndarray): The output data from the YOLOv5 model.
+
+    Returns:
+        tuple: A tuple containing the detected class IDs, confidences, and bounding boxes.
+
+    """
     class_ids = []
     confidences = []
     boxes = []
@@ -110,7 +171,16 @@ def wrap_detection(input_image, output_data):
 
 
 def format_yolov5(frame):
+    """
+    Formats the input frame to have a square shape.
 
+    Args:
+        frame: The input frame to be formatted.
+
+    Returns:
+        The formatted frame with a square shape.
+
+    """
     row, col, _ = frame.shape
     _max = max(col, row)
     result = np.zeros((_max, _max, 3), np.uint8)
@@ -218,5 +288,16 @@ def process_video(video_path):
 
 
 if __name__ == "__main__":
+    args = get_args()
+    if args.input:
+        if args.input.endswith(".jpg") or args.input.endswith(".png"):
+            process_image(args.input)
+        elif args.input.endswith(".mp4") or args.input.endswith(".avi"):
+            process_video(args.input)
+        else:
+            print("Invalid file type. Supported types: jpg, png, mp4, avi")
+    else:
+        print("No input file provided.")
+
     # process_image("D:\CodeWorkSpace\OpenCV-QRCodeDetector-Sample-main\image1.png")
-    process_video("D:\CodeWorkSpace\OpenCV-QRCodeDetector-Sample-main\QQ-video1.mp4")
+    # process_video("D:\CodeWorkSpace\OpenCV-QRCodeDetector-Sample-main\QQ-video1.mp4")
